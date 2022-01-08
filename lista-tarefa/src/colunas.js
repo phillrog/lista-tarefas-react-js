e8f48e4b63e8f24a9d76c1e6a36472f30b329441
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-07 21:39:54 -0300

    Criado o mock e as colunas

---
+++ lista-tarefa/src/colunas.js
@@ -0,0 +1,14 @@
+export const columnsFromBackend = {
+    [uuidv4()]: {
+      title: "A Fazer",
+      items: data,
+    },
+    [uuidv4()]: {
+      title: "Fazendo",
+      items: [],
+    },
+    [uuidv4()]: {
+      title: "Feito",
+      items: [],
+    },
+  }
---
+++ lista-tarefa/src/data-mock.js
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from "uuid"
+export const data = [
+  {
+    id: "1",
+    Task: "Estudar",
+    Due_Date: "08-Janeiro-2022",
+  },
+  {
+    id: "2",
+    Task: "Trabalhar",
+    Due_Date: "09-Janeiro-2022",
+  },
+  {
+    id: "3",
+    Task: "Construir novo app",
+    Due_Date: "10-Janeiro-2022",
+  },
+  {
+    id: "4",
+    Task: "Criar api",
+    Due_Date: "4-Aug-2022",
+  }
+]
