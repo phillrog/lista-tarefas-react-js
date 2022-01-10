d4ea443d65847e6fc43c399d4c39f668906a7e41
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 19:36:49 -0300

    Implementado abstração dos comandos

---
+++ src/building blocks/ListaTarefas.Core/Messages/Command.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using MediatR;
+
+namespace ListaTarefas.Core.Messages
+{
+    public abstract class Command: Message, IRequest<ValidationResult>
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+        public Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/building blocks/ListaTarefas.Core/Messages/CommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using ListaTarefas.Core.Data;
+
+namespace ListaTarefas.Core.Messages
+{
+    public class CommandHandler
+    {
+        protected ValidationResult ValidationResult;
+
+        protected CommandHandler()
+        {
+            ValidationResult = new ValidationResult();
+        }
+
+        protected void AdicionarErro(string message)
+        {
+            ValidationResult.Errors.Add(new ValidationFailure(string.Empty, message));
+        }
+
+        protected async Task<ValidationResult> PersistirDados(IUnitOfWork unitOfWork)
+        {
+            if (!await unitOfWork.Commit()) AdicionarErro("Houve um erro ao persistir os dados");
+
+            return ValidationResult;
+        }
+    }
+}
