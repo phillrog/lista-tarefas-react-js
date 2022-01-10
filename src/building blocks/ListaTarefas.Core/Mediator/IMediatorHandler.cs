5e35796146a346af061d8c4e02017792000034d3
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 20:50:08 -0300

    Implementado a publicação de eventos

--- src/building blocks/ListaTarefas.Core/Mediator/IMediatorHandler.cs
+++ src/building blocks/ListaTarefas.Core/Mediator/IMediatorHandler.cs
@@ -6,5 +6,6 @@ namespace ListaTarefas.Core.Mediator
     public interface IMediatorHandler
     {
         Task<ValidationResult> EnviarComando<T>(T comando) where T : Command;
+        Task PublicarEvento<T>(T evento) where T : Event;
     }
 }
--- src/building blocks/ListaTarefas.Core/Mediator/MediatorHandler.cs
+++ src/building blocks/ListaTarefas.Core/Mediator/MediatorHandler.cs
@@ -16,6 +16,11 @@ namespace ListaTarefas.Core.Mediator
         public async Task<ValidationResult> EnviarComando<T>(T comando) where T : Command
         {
             return await _mediator.Send(comando);
-        }        
+        }
+
+        public async Task PublicarEvento<T>(T evento) where T : Event
+        {
+            await _mediator.Publish(evento); 
+        }
     }
 }
