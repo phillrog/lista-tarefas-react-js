f8b00b2bcbdd80deb0403c63ad4e4feccf4e9f51
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 17:41:10 -0300

    Criado o contexto

---
+++ src/building blocks/ListaTarefas.Core/Data/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace ListaTarefas.Core.Data
+{
+    public interface IUnitOfWork
+    {
+        Task<bool> Commit();
+    }
+}
---
+++ src/services/ListaTarefas.Infra/ListaTarefaContext.cs
@@ -0,0 +1,29 @@
+﻿using ListaTarefas.Core.Data;
+using ListaTarefas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefas.Infra
+{
+    public class ListaTarefaContext : DbContext, IUnitOfWork
+    {
+        public ListaTarefaContext(DbContextOptions<ListaTarefaContext> options)
+            :base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ListaTarefaContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
