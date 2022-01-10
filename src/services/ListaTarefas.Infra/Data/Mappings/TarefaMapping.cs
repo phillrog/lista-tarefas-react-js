3e6eaa0f4068e7b1d1639e1ee04819e2166652a0
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 16:54:20 -0300

    foi mapeado a entidade

---
+++ src/services/ListaTarefas.Infra/Data/Mappings/TarefaMapping.cs
@@ -0,0 +1,18 @@
+﻿using ListaTarefas.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ListaTarefas.Infra.Data.Mappings
+{
+    public class TarefaMapping : IEntityTypeConfiguration<Tarefa>
+    {
+        public void Configure(EntityTypeBuilder<Tarefa> builder)
+        {
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Descricao).HasMaxLength(250);
+            builder.Property(s => s.Vencimento);
+            builder.Property(s => s.Status);
+        }
+    }
+}
