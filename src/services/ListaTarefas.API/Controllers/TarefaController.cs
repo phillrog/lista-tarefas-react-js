a907d4faab77037aa6af383d3c4c7d7fc9251ebb
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 19:37:32 -0300

    Criado o controller

---
+++ src/services/ListaTarefas.API/Controllers/TarefaController.cs
@@ -0,0 +1,26 @@
+﻿using ListaTarefas.Application.Commands;
+using ListaTarefas.Core.Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaTarefas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+        private readonly IMediatorHandler _mediator;
+
+        public TarefaController(IMediatorHandler mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("nova-tarefa")]
+        public async Task<IActionResult> CadastrarTarefa(CadastrarTarefaCommand comando)
+        {
+            var result = await _mediator.EnviarComando<CadastrarTarefaCommand>(comando);
+            
+            return Ok();
+        }
+    }
+}
