6afedf0d564d4dadae58310e79ea99bb747fe849
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 19:37:16 -0300

    Foi criado o comando cadastrar

---
+++ src/services/ListaTarefas.Application/Commands/CadastrarTarefaCommand.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using ListaTarefas.Core.Messages;
+
+namespace ListaTarefas.Application.Commands
+{
+    public class CadastrarTarefaCommand : Command
+    {
+        public string Descricao { get; private set; }
+        public DateTime Vencimento { get; private set; }
+    }
+
+    public class CadastrarTarefaValidation : AbstractValidator<CadastrarTarefaCommand>
+    {
+        public CadastrarTarefaValidation()
+        {
+            RuleFor(m => m.Descricao)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(3)
+                .MaximumLength(250)
+                .WithMessage("Descrição inválida");
+
+            RuleFor(m => m.Vencimento)
+                .NotNull()
+                .WithMessage("Data de vencimento inválida");
+        }
+    }
+}
---
+++ src/services/ListaTarefas.Application/Commands/TarefaHandler.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Results;
+using ListaTarefas.Core.Messages;
+using MediatR;
+
+namespace ListaTarefas.Application.Commands
+{
+    public class TarefaHandler : CommandHandler, IRequestHandler<CadastrarTarefaCommand, ValidationResult>
+    {
+        public Task<ValidationResult> Handle(CadastrarTarefaCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
