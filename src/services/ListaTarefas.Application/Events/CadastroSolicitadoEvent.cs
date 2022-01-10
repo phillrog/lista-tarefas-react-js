2fdb5cf3a62733919f61dbfc24b77280066c7fa8
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 20:50:43 -0300

    Foi criado o event handler de tarefas

---
+++ src/services/ListaTarefas.Application/Events/CadastroSolicitadoEvent.cs
@@ -0,0 +1,16 @@
+﻿using ListaTarefas.Core.Messages;
+
+namespace ListaTarefas.Application.Events
+{
+    public class CadastroSolicitadoEvent : Event
+    {
+        public string Descricao { get; private set; }
+        public DateTime Vencimento { get; private set; }
+        public CadastroSolicitadoEvent(string descricao, DateTime vencimento)
+        {
+            Descricao = descricao;
+            Vencimento = vencimento;
+            AggregateId = Guid.NewGuid();
+        }
+    }
+}
---
+++ src/services/ListaTarefas.Application/Events/TarefaEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaTarefas.Application.Events
+{
+    public class TarefaEventHandler : INotificationHandler<CadastroSolicitadoEvent>
+    {
+        public async Task Handle(CadastroSolicitadoEvent notification, CancellationToken cancellationToken)
+        {
+            Task.CompletedTask;
+        }
+    }
+}
