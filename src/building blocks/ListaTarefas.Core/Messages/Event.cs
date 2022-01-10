d382380813384b814a5319f6a835b8a0f7b54c1b
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 20:50:20 -0300

    Criado a abstração do evento

---
+++ src/building blocks/ListaTarefas.Core/Messages/Event.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace ListaTarefas.Core.Messages
+{
+    public class Event : Message, INotification
+	{
+		public DateTime Timestamp { get; private set; }
+		public Type Tipo { get; set; }
+
+		protected Event()
+		{
+			Timestamp = DateTime.Now;
+		}
+	}
+}
