aea9ddd29311e3f1965818deb8c53bc2c8f0bf66
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 21:41:36 -0300

    Implementação do consumidor

---
+++ src/services/ListaTarefas.Application/Consumers/CadastroSolicitadoConsumer.cs
@@ -0,0 +1,13 @@
+﻿using ListaTarefas.Application.Contracts;
+using MassTransit;
+
+namespace ListaTarefas.Application.Consumers
+{
+    public class CadastroSolicitadoConsumer : IConsumer<ICadastroSolicitado>
+    {
+        public async Task Consume(ConsumeContext<ICadastroSolicitado> context)
+        {
+           await Task.CompletedTask;
+        }
+    }
+}
