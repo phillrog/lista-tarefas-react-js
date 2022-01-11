5a4afee1dd70ae317af3ee6b34a4377cfd210d33
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-11 20:43:20 -0300

    Criação do contrato

---
+++ src/services/ListaTarefas.Application/Contracts/IRemocaoSolicitada.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaTarefas.Application.Contracts
+{
+    public interface IRemocaoSolicitada
+    {
+        public Guid AggregateId { get; }
+        public Guid Id { get; set; }        
+        public DateTime Timestamp { get; }
+        public string MessageType { get; set; }
+    }
+}
