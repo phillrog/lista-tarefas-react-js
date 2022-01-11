839f53ac9919982f8259b86ac20d3c5ae210ca52
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-11 19:07:28 -0300

    Criado o contrato de edição

---
+++ src/services/ListaTarefas.Application/Contracts/IEdicaoSolicitada.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaTarefas.Application.Contracts
+{
+    public interface IEdicaoSolicitada
+    {
+        public Guid AggregateId { get; }
+        public Guid Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Vencimento { get; set; }
+        public int Status { get; set; }
+        public DateTime Timestamp { get; }
+        public string MessageType { get; set; }
+    }
+}
