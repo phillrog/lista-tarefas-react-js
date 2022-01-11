b54eef5b4f280c16e528db1cb7446fd709968f47
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-10 22:22:21 -0300

    Foi adicionado mais campos a entidaide

--- src/services/ListaTarefas.Infra/Migrations/20220111011918_CamposNovos.Designer.cs
+++ src/services/ListaTarefas.Infra/Migrations/20220111011918_CamposNovos.Designer.cs
@@ -4,6 +4,7 @@ using ListaTarefas.Infra;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ListaTarefas.Infra.Migrations
 {
     [DbContext(typeof(ListaTarefaContext))]
-    partial class ListaTarefaContextModelSnapshot : ModelSnapshot
+    [Migration("20220111011918_CamposNovos")]
+    partial class CamposNovos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/services/ListaTarefas.Infra/Migrations/20220111011918_CamposNovos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ListaTarefas.Infra.Migrations
+{
+    public partial class CamposNovos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Tarefas",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 250);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataAtualizacao",
+                table: "Tarefas",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCadastro",
+                table: "Tarefas",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataAtualizacao",
+                table: "Tarefas");
+
+            migrationBuilder.DropColumn(
+                name: "DataCadastro",
+                table: "Tarefas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Descricao",
+                table: "Tarefas",
+                type: "int",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
