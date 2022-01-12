fd4f72c88f5231e83f570cabd3b26183ed57bb9d
Phillipe Roger Souza <phillrog@hotmail.com>
2022-01-11 22:36:02 -0300

    Response da query

--- src/building blocks/ListaTarefas.Core/Communication/ResponseResult.cs
+++ src/building blocks/ListaTarefas.Core/Communication/ResponseResult.cs
@@ -3,6 +3,17 @@
     public class ResponseResult
     {
         public int Status { get; set; }
-        public string Message { get; set; }
-    }    
+        public string Message { get; set; }        
+    }
+
+    public class QueryResult { }
+
+    public class ResponseQueryResult
+    {
+        public List<QueryResult> Data { get; set; }
+        public ResponseQueryResult()
+        {
+            Data = new List<QueryResult>();
+        }
+    }
 }
