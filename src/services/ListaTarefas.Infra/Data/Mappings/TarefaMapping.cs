4cb240ab310a569e192c6fa0c7959077185f01d5
phillrog <phillrog@hotmail.com>
2022-03-15 21:25:24 -0300

    Foi criado um conversor para o ef transformar o enum em string

--- src/services/ListaTarefas.Infra/Data/Mappings/TarefaMapping.cs
+++ src/services/ListaTarefas.Infra/Data/Mappings/TarefaMapping.cs
@@ -1,6 +1,8 @@
 ﻿using ListaTarefas.Domain.Entities;
+using ListaTarefas.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ListaTarefas.Infra.Data.Mappings
 {
@@ -12,9 +14,34 @@ namespace ListaTarefas.Infra.Data.Mappings
 
             builder.Property(s => s.Descricao).HasMaxLength(250);
             builder.Property(s => s.Vencimento);
-            builder.Property(s => s.Status);
+            builder.Property(s => s.Status).HasMaxLength(10).HasConversion(new StatusConversorCustomizado());
             builder.Property(s => s.DataCadastro);
             builder.Property(s => s.DataAtualizacao);
+        }
+    }
+
+    public class StatusConversorCustomizado : ValueConverter<StatusEnum, string>
+    {
+        public StatusConversorCustomizado() : base(
+            p => ConverterParaOhBancoDeDados(p),
+            value => ConverterParaAplicacao(value),
+            new ConverterMappingHints(1))
+        {
+
+        }
+
+        static string ConverterParaOhBancoDeDados(StatusEnum status)
+        {
+            return status.ToString();
+        }
+
+        static StatusEnum ConverterParaAplicacao(string value)
+        {
+            var status = Enum
+                .GetValues<StatusEnum>()
+                .FirstOrDefault(p => p.ToString() == value);
+
+            return status;
         }
     }
 }
