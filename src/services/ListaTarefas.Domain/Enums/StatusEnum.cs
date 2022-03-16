8a5800964e9c1b74e1e7f7ae8893cc7182a4f636
phillrog <phillrog@hotmail.com>
2022-03-15 21:23:35 -0300

    Foi definido o valor dos enumeradores

--- src/services/ListaTarefas.Domain/Enums/StatusEnum.cs
+++ src/services/ListaTarefas.Domain/Enums/StatusEnum.cs
@@ -2,9 +2,9 @@
 {
     public enum StatusEnum
     {
-        AFazer,
-        Fazendo,
-        Feito,
-        Cancelada
+        AFazer = 0,
+        Fazendo = 1,
+        Feito = 2,
+        Cancelada = 3
     }
 }
