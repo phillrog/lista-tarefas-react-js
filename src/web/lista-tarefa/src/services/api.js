2cb0ed352c40d5928e56c4e30c362e570b395cc0
phillrog <phillrog@hotmail.com>
2022-03-21 20:52:28 -0300

    Implementado o metodo de deletar

--- src/web/lista-tarefa/src/services/api.js
+++ src/web/lista-tarefa/src/services/api.js
@@ -8,27 +8,35 @@ const quadroTarefa = ['A Fazer', 'Fazendo', 'Feito'];
 
 const api = {
   novaTarefa: (descricao, vencimento, quadro) => {
-
     tarefaController.post("nova-tarefa", {
       descricao,
       vencimento,
       status: quadroTarefa.indexOf(quadro)
     })
-    .then((response) => console.log(response))
-    .catch((err) => {
-      console.error("ops! ocorreu um erro" + err);
-    });
+      .then((response) => console.log(response))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
   },
-  listar: async() => {
+  listar: async () => {
     return await tarefaController.get("listar")
-    .then((response) => {
-      if(response.status === 200) return response.data.data;
-      else return [];
-    })
-    .catch((err) => {
-      console.error("ops! ocorreu um erro" + err);
-    });
-  } 
+      .then((response) => {
+        if (response.status === 200) return response.data;
+        else return [];
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  },
+  deletar: async (id) => {
+    return await tarefaController.delete(`remover-tarefa/${id}`)
+      .then((response) => {
+        if (response.status === 200) window.alert('Tarefa removida com sucesso! '); 
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  },
 };
 
 export default api;
